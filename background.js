58a4bd1506fdf3475bd1becb95e26c6aa279d54d
xeandr <xeandr@gmail.com>
2013-10-22 21:54:43 +0300

    Added jquery support

--- background.js
+++ background.js
@@ -3,9 +3,18 @@ chrome.runtime.onMessage.addListener(onMessage);
 function onMessage(request, sender, sendResponse) {
     switch (request.type) {
         case "GAME_DATA":
+            onGameDataReceived(sender, request.data);
             sendResponse({message: "Got the game_data, thanks!"});
             break;
         default:
             sendResponse({message: "What is this crap?"});
     }
+}
+
+
+function onGameDataReceived(sender, game_data) {
+    console.log("Background script received game data: ", game_data);
+    chrome.tabs.sendMessage(sender.tab.id, {type: "SCREEN", data: game_data.screen, game_data: game_data}, function(response) {
+        console.log(response.message);
+    });
 }
